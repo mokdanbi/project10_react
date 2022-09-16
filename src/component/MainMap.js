ed5a11bd75f0061ccde508c95ae618c3f2a5c831
mokdanbi <l26tempo@naver.com>
2022-09-16 16:10:28 +0900

    0916

--- src/component/MainMap.js
+++ src/component/MainMap.js
@@ -1,12 +1,13 @@
-import { Map, MapMarker } from "react-kakao-maps-sdk";
+// import { Map, MapMarker } from "react-kakao-maps-sdk";
 
 const MainMap = () => {
     return (
         <section className="MainMap">
             <div className="container">
-                <Map center={{ lat: 37.4987, lng: 127.0365 }} style={{ width: "775px", height: "400px" }}>
+                {/* <Map center={{ lat: 37.4987, lng: 127.0365 }} style={{ width: "775px", height: "400px" }}>
                     <MapMarker position={{ lat: 37.4987, lng: 127.0365 }} ></MapMarker>
-                </Map>
+                </Map> */}
+                <div className="mapImg"></div>
                 <div className="mapTxt">
                     <h2>MIDAS<br />OFFICE</h2>
                     <p>60, Yeoksam-ro 17-gil,<br />
