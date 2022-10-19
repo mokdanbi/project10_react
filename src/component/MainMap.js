c13bfc49ca7be5adc04f8bd919ef02ea294b2afb
mokdanbi <l26tempo@naver.com>
2022-10-19 11:39:12 +0900

    1019

--- src/component/MainMap.js
+++ src/component/MainMap.js
@@ -1,22 +1,22 @@
-// import { Map, MapMarker } from "react-kakao-maps-sdk";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
 
 const MainMap = () => {
-    return (
-        <section className="MainMap">
-            <div className="container">
-                {/* <Map center={{ lat: 37.4987, lng: 127.0365 }} style={{ width: "775px", height: "400px" }}>
-                    <MapMarker position={{ lat: 37.4987, lng: 127.0365 }} ></MapMarker>
-                </Map> */}
-                <div className="mapImg"></div>
-                <div className="mapTxt">
-                    <h2>MIDAS<br />OFFICE</h2>
-                    <p>60, Yeoksam-ro 17-gil,<br />
-                        Gangnam-gu,<br />
-                        Seoul, Korea</p>
-                </div>
-            </div>
-        </section >
-    )
+  return (
+    <section className="MainMap">
+      <div className="container">
+        {/* <Map center={{ lat: 37.4987, lng: 127.0365 }} style={{ width: "775px", height: "400px" }}>
+          <MapMarker position={{ lat: 37.4987, lng: 127.0365 }} ></MapMarker>
+        </Map> */}
+        <div className="mapImg"></div>
+        <div className="mapTxt">
+          <h2>MIDAS<br />OFFICE</h2>
+          <p>60, Yeoksam-ro 17-gil,<br />
+            Gangnam-gu,<br />
+            Seoul, Korea</p>
+        </div>
+      </div>
+    </section >
+  )
 }
 
 export default MainMap;
