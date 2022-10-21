79df4ced0b273702e20056fd80c0d4b75ff45a1d
mokdanbi <l26tempo@naver.com>
2022-10-21 17:40:18 +0900

    10121

--- src/component/MainMap.js
+++ src/component/MainMap.js
@@ -4,10 +4,10 @@ const MainMap = () => {
   return (
     <section className="MainMap">
       <div className="container">
-        {/* <Map center={{ lat: 37.4987, lng: 127.0365 }} style={{ width: "775px", height: "400px" }}>
+        <Map center={{ lat: 37.4987, lng: 127.0365 }} style={{ width: "775px", height: "400px" }}>
           <MapMarker position={{ lat: 37.4987, lng: 127.0365 }} ></MapMarker>
-        </Map> */}
-        <div className="mapImg"></div>
+        </Map>
+        {/* <div className="mapImg"></div> */}
         <div className="mapTxt">
           <h2>MIDAS<br />OFFICE</h2>
           <p>60, Yeoksam-ro 17-gil,<br />
