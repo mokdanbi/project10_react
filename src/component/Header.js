beb69aa23c18f0b48bc677532b8cd2ae0796d94d
mokdanbi <l26tempo@naver.com>
2022-10-24 09:26:54 +0900

    1024

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,35 +1,37 @@
+import { Link } from "react-router-dom";
+
 const NAVLINK = [
-    { id: 1, menu: "COMPANY", link: "/1" },
-    { id: 2, menu: "WORK", link: "/2" },
-    { id: 3, menu: "ESTIMATE", link: "/3" },
-    { id: 4, menu: "PR", link: "/4" },
+  { id: 1, menu: "COMPANY", link: "/1" },
+  { id: 2, menu: "WORK", link: "/2" },
+  { id: 3, menu: "ESTIMATE", link: "/3" },
+  { id: 4, menu: "PR", link: "/4" },
 ]
 
 const Header = () => {
-    return (
-        <header className="Header">
-            <div className="container">
-                <h1>
-                    <a href="">
-                        MIDAS
-                    </a>
-                </h1>
-                <nav className="Gnb">
-                    <ul>
-                        {
-                            NAVLINK.map((it, idx) => {
-                                return (
-                                    <li key={it.id}>
-                                        <a href={it.link}>{it.menu}</a>
-                                    </li>
-                                )
-                            })
-                        }
-                    </ul>
-                </nav>
-            </div>
-        </header>
-    )
+  return (
+    <header className="Header">
+      <div className="container">
+        <h1>
+          <Link to="./component/Header">
+            MIDAS
+          </Link>
+        </h1>
+        <nav className="Gnb">
+          <ul>
+            {
+              NAVLINK.map((it, idx) => {
+                return (
+                  <li key={it.id}>
+                    <a href={it.link}>{it.menu}</a>
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
 }
 
 export default Header;
